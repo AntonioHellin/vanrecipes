3e5c3395ec1f1841460320fc98152d9f88b32b7e
AntonioHellin <antonio.hellinh@gmail.com>
2023-08-24 21:55:25 +0200

    Initial commit for VanRecipes

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e22827da-1a9c-4b78-b4b8-5ef3e2706d9b';
+export const MODAL_CLOSE_SEC = 2.5;
